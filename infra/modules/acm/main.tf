56b51d46e9bacf78d30164d3020513b4d647e76d
Abdiyasir <AbdiyasirM100@gmail.com>
2026-06-21 20:20:23 +0100

    Terraform plan pipeline

--- infra/modules/acm/main.tf
+++ infra/modules/acm/main.tf
@@ -26,7 +26,7 @@ resource "aws_route53_record" "www" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.acm_hosted_zone.zone_id
+  zone_id         = data.aws_route53_zone.hosted_zone.zone_id
 }
 
 resource "aws_acm_certificate_validation" "cert" {
