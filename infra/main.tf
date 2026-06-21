bddb0b346a5850e8da922d6068b9bbef328bf383
Abdiyasir <AbdiyasirM100@gmail.com>
2026-06-21 21:02:59 +0100

    Terraform plan Pipeline

--- infra/main.tf
+++ infra/main.tf
@@ -83,7 +83,7 @@ module "alb" {
   api_port        = var.api_port
   vpc_id          = module.vpc.vpc_id
   public_subnet_1 = module.vpc.public_subnet_1
-  public_subnet_2 = module.vpc.public_subnet_2
+  public_subnet_2 = module.vpc.private_subnet_2
   alb_sg          = module.vpc.alb_sg
   dashboard_port  = var.dashboard_port
 }
