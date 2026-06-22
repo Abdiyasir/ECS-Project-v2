5ed002e6ace74491404e4536d8a8be938c4d444f
Abdiyasir <AbdiyasirM100@gmail.com>
2026-06-22 21:12:49 +0100

    terraform destroy

--- infra/modules/acm/main.tf
+++ infra/modules/acm/main.tf
@@ -12,7 +12,7 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "cert_validation" {
   for_each = {
     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -31,5 +31,5 @@ resource "aws_route53_record" "www" {
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.www : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
